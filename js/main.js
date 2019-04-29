1c149d20e5721825d80b6f9a698eaa49514e6282
Tremblay Julien <43877453+JulienWebDeveloppeur@users.noreply.github.com>
2019-04-29 15:22:55 +0200

    maj

--- js/main.js
+++ js/main.js
@@ -35,12 +35,12 @@ function Hero(game, x, y) {
 Hero.prototype = Object.create(Phaser.Sprite.prototype);
 Hero.prototype.constructor = Hero;
 
-Hero.prototype.move = function(direction) {
+Hero.prototype.move = function (direction) {
     const SPEED = 200;
     this.body.velocity.x = direction * SPEED;
 };
 
-Hero.prototype.jump = function() {
+Hero.prototype.jump = function () {
     const JUMP_SPEED = 500;
     let canJump = this.body.touching.down;
 
@@ -57,7 +57,7 @@ Hero.prototype.jump = function() {
 
 PlayState = {};
 
-PlayState.init = function() {
+PlayState.init = function () {
     this.game.renderer.renderSession.roundPixels = true;
 
     this.keys = this.game.input.keyboard.addKeys({
@@ -66,7 +66,7 @@ PlayState.init = function() {
         up: Phaser.KeyCode.UP
     });
 
-    this.keys.up.onDown.add(function() {
+    this.keys.up.onDown.add(function () {
         let didJump = mage.jump();
         if (didJump) {
             this.sfx.jump.play();
@@ -74,7 +74,7 @@ PlayState.init = function() {
     }, this);
 };
 
-PlayState.preload = function() {
+PlayState.preload = function () {
     this.game.load.json('level:1', 'data/level01.json');
     this.game.load.image('background', 'images/background.png');
     this.game.load.image('background1', 'images/background1.png');
@@ -83,19 +83,26 @@ PlayState.preload = function() {
     this.game.load.image('grass:2x1', 'images/grass_2x1.png');
     this.game.load.image('grass:2x1:noborder', 'images/grass_2x1_noborder.png');
     this.game.load.image('lava', 'images/lava.png');
+    this.game.load.image('lava_bouncing', 'images/lava.png');
     this.game.load.image('doorClosed', 'images/door-closed.png');
     this.game.load.image('echelle', 'images/echelle.png');
     this.game.load.image('mage', 'images/mage_stopped.png');
     this.game.load.image('mage1', 'images/mage1_stopped.png');
+    this.game.load.image('castle', 'images/castle.png');
     this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
     this.game.load.audio('sfx:coin', 'audio/coin.wav');
 };
 var map;
-var movingGrasseY
-var movingGrasseX
+var movingGrasseY;
+var movingGrasseX;
+var movingGrasseXCastle;
+var echelle1;
+var echelle2;
 var block;
-PlayState.create = function() {
+var lava;
+var lavaBouncing;
+PlayState.create = function () {
 
     // create sound entities
     this.sfx = {
@@ -111,6 +118,7 @@ PlayState.create = function() {
         }
     }, 300)
 
+
     function stars() {
         if (timeMap % 2 == 0) {
             map.loadTexture('background1', 0);
@@ -123,29 +131,39 @@ PlayState.create = function() {
 };
 
 var timeMap = 10000;
-PlayState.update = function() {
+PlayState.update = function () {
 
     this._handleCollisions();
     this._handleInput();
-
+    if ((mage.position.y > 300 && mage.position.y < 400) && (mage.position.x > 40 && mage.position.x < 60)) {
+        if (this.keys.up.isDown) { // move hero left
+            mage.position.y -= 100;
+        }
+    }
 };
 
-function spriteVsMovinPlatform(mage, platForm) {
-    //mage.body.y -= platForm.body.y - platForm.body.prev.y;
-    //mage.body.y = platForm.body.y - platForm.body.prev.y;
-    console.log("mage y: " + mage.body.y)
-    console.log("platform y: " + platForm.body.y)
+function spriteVsMovinPlatformY(mage, platForm) {
+    mage.body.y = platForm.body.y - mage.body.height;
+    console.log("mage y: " + mage.body.y);
+    console.log("platform y: " + platForm.body.y);
 }
 
-PlayState._handleCollisions = function() {
+function spriteVsMovinPlatformX(mage, platForm) {
+    mage.body.y = platForm.body.y - mage.body.height;
+    mage.body.x = platForm.body.x;
+}
+
+PlayState._handleCollisions = function () {
 
     this.game.physics.arcade.collide(mage, this.platforms);
     this.game.physics.arcade.overlap(mage, this.coins, this._onHeroVsCoin,
         null, this);
-    this.physics.arcade.collide(mage, movingGrasseY, spriteVsMovinPlatform, null, this);
+    this.physics.arcade.collide(mage, movingGrasseY, spriteVsMovinPlatformY, null, this);
+    this.physics.arcade.collide(mage, movingGrasseX, spriteVsMovinPlatformX, null, this);
+    this.physics.arcade.collide(mage, movingGrasseXCastle, spriteVsMovinPlatformX, null, this);
 };
 
-PlayState._handleInput = function() {
+PlayState._handleInput = function () {
     if (this.keys.left.isDown) { // move hero left
         mage.move(-1);
     } else if (this.keys.right.isDown) { // move hero right
@@ -155,7 +173,7 @@ PlayState._handleInput = function() {
     }
 };
 
-PlayState._loadLevel = function(data) {
+PlayState._loadLevel = function (data) {
     // create all the groups/layers that we need
     this.platforms = this.game.add.group();
 
@@ -164,6 +182,10 @@ PlayState._loadLevel = function(data) {
 
     movingGrasseY = this.platformsMovable.create(105, 560, 'grass:2x1');
     movingGrasseX = this.platformsMovable.create(250, 250, 'grass:2x1');
+    movingGrasseXCastle = this.platformsMovable.create(760, 570, 'grass:2x1');
+
+    echelle1 = this.game.add.sprite(40, 304, 'echelle');
+    echelle2 = this.game.add.sprite(40, 364, 'echelle');
 
     this.platformsMovable.setAll('body.allowGravity', false);
     this.platformsMovable.setAll('body.immovable', true);
@@ -178,6 +200,11 @@ PlayState._loadLevel = function(data) {
     this.game.add.tween(movingGrasseX).to({
         x: movingGrasseX.position.x + 130
     }, 2000, Phaser.Easing.Linear.None, true, 0, -1, true);
+
+    movingGrasseX.body.setSize(movingGrasseXCastle.width, movingGrasseXCastle.height);
+    this.game.add.tween(movingGrasseXCastle).to({
+        x: movingGrasseXCastle.position.x + 250
+    }, 4000, Phaser.Easing.Linear.None, true, 0, -1, true);
 
 
     // spawn all platforms
@@ -196,7 +223,7 @@ PlayState._loadLevel = function(data) {
     this.game.physics.arcade.gravity.y = GRAVITY;
 };
 
-PlayState._spawnPlatform = function(platform) {
+PlayState._spawnPlatform = function (platform) {
     let sprite = this.platforms.create(
         platform.x, platform.y, platform.image);
     this.game.physics.enable(sprite);
@@ -207,7 +234,7 @@ PlayState._spawnPlatform = function(platform) {
 var mage;
 var timeMage = 10000;
 
-PlayState._spawnCharacters = function(data) {
+PlayState._spawnCharacters = function (data) {
     // spawn hero
     mage = new Hero(this.game, data.hero.x, data.hero.y);
     setInterval(() => {
@@ -231,7 +258,7 @@ PlayState._spawnCharacters = function(data) {
 
 };
 
-PlayState._spawnCoin = function(coin) {
+PlayState._spawnCoin = function (coin) {
     let sprite = this.coins.create(coin.x, coin.y, 'coin');
     sprite.anchor.set(0.5, 0.5);
 
@@ -242,7 +269,7 @@ PlayState._spawnCoin = function(coin) {
     sprite.animations.play('rotate');
 };
 
-PlayState._onHeroVsCoin = function(hero, coin) {
+PlayState._onHeroVsCoin = function (hero, coin) {
     this.sfx.coin.play();
     coin.kill();
 };
@@ -251,7 +278,7 @@ PlayState._onHeroVsCoin = function(hero, coin) {
 // entry point
 // =============================================================================
 
-window.onload = function() {
+window.onload = function () {
     var game = new Phaser.Game(config);
     game.state.add('play', PlayState);
     game.state.start('play');
