a4ca18ea47057ce894471633a04c3f813b519b57
Antoine Meliodas <calmettes.antoine2@gmail.com>
2019-05-05 09:50:08 +0200

    Son BG Only Firefox

--- js/music.js
+++ js/music.js
@@ -1,10 +1,21 @@
-/*var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-    if(!isChrome){
-      $('#iframeAudio').remove()
-    }
-  else{
-     $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background
-  } */
-var audio = new Audio("./audio/bgm.mp3");
-audio.play();
-audio.volume = 0.1;
+let player1 = document.getElementById("iframeAudio");
+let player2 = document.getElementById("playAudio");
+
+player2.volume = 0.3;
+
+
+
+
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+//     if(!isChrome){
+//       $('#iframeAudio').volume(0)
+      
+//       $('#iframeAudio').remove()
+//     }
+//   else{
+//     $('#playAudio').volume(0)
+//      $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background
+//   } ;
+
+
+
