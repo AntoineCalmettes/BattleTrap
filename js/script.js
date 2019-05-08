8bc66c67c5604f2b7c9da0816095a4a1bf5ff378
Antoine Meliodas <calmettes.antoine2@gmail.com>
2019-05-08 18:27:22 +0200

    GIF MAGE + TEXT DESCRIPTION

--- js/script.js
+++ js/script.js
@@ -60,7 +60,7 @@ mageChoose.addEventListener("click", () => {
     progressSpeedAttaque.ariaValuemax="100"
     progressSpeedAttaque.style.width="50%"
 
-    historyPerso.innerHTML=" Largement considéré comme l'un des plus puissants sorciers de Runeterra, Ce mage est un archimage endurci par les ans qui porte un fardeau incommensurable sur ses épaules. Armé d'une constitution à toute épreuve et d'un vaste arsenal de connaissances mystiques, il consacre sa vie à rassembler les Runes telluriques, les fragments de magie brute qui ont jadis servi à façonner le monde.";
+    historyPerso.innerHTML=" Largement considéré comme l'un des plus puissants sorciers de Vallaria, Ce mage est un archimage endurci par les ans qui porte un fardeau incommensurable sur ses épaules. Armé d'une constitution à toute épreuve et d'un vaste arsenal de connaissances mystiques, il consacre sa vie à rassembler les Runes telluriques, les fragments de magie brute qui ont jadis servi à façonner le monde.";
 
 
 }, false)
@@ -90,7 +90,7 @@ voleurChoose.addEventListener("click", () => {
     progressSpeedAttaque.ariaValuemax="100"
     progressSpeedAttaque.style.width="100%"
 
-    historyPerso.innerHTML=" On dit que la mort n'est pas drôle. C'est vrai, à moins que vous ne soyez ce voleur ; là, elle est hilarante. C'est le premier comique meurtrier en activité de Valorane. Celui qu'on appelle le voleur des ténèbres est une énigme. Personne de vivant ne sait d'où il vient, lui-même n'en parle jamais à personne. Les rumeurs disent qu'il n'est pas de Runeterra, qu'il est un être originaire d'un monde sombre et difforme.";
+    historyPerso.innerHTML=" On dit que la mort n'est pas drôle. C'est vrai, à moins que vous ne soyez ce voleur ; là, elle est hilarante. C'est le premier comique meurtrier en activité de ValoraneCity. Celui qu'on appelle le voleur des ténèbres est une énigme. Personne de vivant ne sait d'où il vient, lui-même n'en parle jamais à personne. Les rumeurs disent qu'il n'est pas de RunerraCity, qu'il est un être originaire d'un monde sombre et difforme.";
 
 }, false)
 
@@ -119,5 +119,5 @@ guerrierChoose.addEventListener("click", () => {
     progressSpeedAttaque.ariaValuemax="100"
     progressSpeedAttaque.style.width="20%"
 
-    historyPerso.innerHTML=" Né dans la noble famille des Crowngard, Ce guerrier sut dès son plus jeune âge que sa vocation était de défendre le trône de Demacia au péril de sa vie. Son père, Piter, était un officier décoré et sa tante Tianna était le Capitaine-Épée d'une unité d'élite, le Détachement hardi : et tous deux étaient appréciés et respectés par le roi Stalas IV. Nul ne doutait que ce guerrier servirait le fils du roi de la même manière.";
+    historyPerso.innerHTML=" Né dans la noble famille des Crown, Ce guerrier sut dès son plus jeune âge que sa vocation était de défendre le trône de Demacia au péril de sa vie. Son père, Piter, était un officier décoré et sa tante Tianna était le Capitaine-Épée d'une unité d'élite, le Détachement hardi : et tous deux étaient appréciés et respectés par le roi Stalas IV. Nul ne doutait que ce guerrier servirait le fils du roi de la même manière.";
 }, false)
